96e943692d93b725689961319d3367a7cb294abe
¨cemalhekim¨ <cemalhekim@icloud.com>
2025-05-19 02:20:39 +0200

    Apply .gitignore and remove ignored files from git tracking

--- dev_ws/build/xarm_controller/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ch/Workspace/llm-driven-robotics/dev_ws/install/xarm_description/share/xarm_description/package.sh"
-. "/home/ch/Workspace/llm-driven-robotics/dev_ws/install/xarm_msgs/share/xarm_msgs/package.sh"
-. "/home/ch/Workspace/llm-driven-robotics/dev_ws/install/xarm_sdk/share/xarm_sdk/package.sh"
-. "/home/ch/Workspace/llm-driven-robotics/dev_ws/install/xarm_api/share/xarm_api/package.sh"

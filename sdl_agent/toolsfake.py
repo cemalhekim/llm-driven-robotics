73d56b6f18eb53e0a53c837d4f4142779585ef45
chekim <cemal.hekim@bam.de>
2025-09-30 17:51:06 +0200

    Add test CSV data and fake measurement functions for OCP and CA

---
+++ sdl_agent/toolsfake.py
@@ -0,0 +1,17 @@
+def ocp_measurement(i: int):
+    print("FAKE: Performing OCP measurement for sample", i)
+
+def ca_measurement(i: int):
+    print("FAKE: Performing CA measurement for sample", i)
+
+def cv_measurement(i: int):
+    print("FAKE: Performing CV measurement for sample", i)
+
+def bring_sample_to_user(i: int):
+    print("FAKE: Bringing sample #", i, "to user")
+
+def collect_sample_from_user(i: int):
+    print("FAKE: Collecting sample #", i, "from user")
+
+def go_home():
+    print("FAKE: Going home")
